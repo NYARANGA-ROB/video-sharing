982b1db0bf9cb162da6652cf52c479587cb1665a
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-09-10 14:51:07 +0300

    Refactor Comments component structure and logic

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom";
 import moment from "moment";
 
 class Comments extends React.Component {
+    
     constructor(props) {
         super(props);
         this.state = {data: []};
